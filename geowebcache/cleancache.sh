6be4bd1b5516e3617c2bb98e8e8d2b0be8cbe1da
Simone Giannecchini <simone.giannecchini@geo-solutions.it>
2015-08-03 12:50:06 +0200

    Update cleancache.sh

--- geowebcache/cleancache.sh
+++ geowebcache/cleancache.sh
@@ -9,6 +9,14 @@
 # cleancache.sh "^topp\\:"
 # cleancache.sh "^topp\\:states$"
 # If regex not present use the standard input to clear layers
+# 
+# 
+# Copyright 2015, GeoSolutions Sas.
+# All rights reserved.
+# 
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+# 
 ##############################################################################
  
 #GEOSERVER ADMINISTRATOR CREDENTIALS
